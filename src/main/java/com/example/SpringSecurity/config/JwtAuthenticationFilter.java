232613821a1103da28c5b8cd160824557430125c
SHREYANSH <shreyanshchichi@gmail.com>
2026-03-04 11:51:32 +0530

    solve some nullable issues

--- src/main/java/com/example/SpringSecurity/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/SpringSecurity/config/JwtAuthenticationFilter.java
@@ -1,13 +1,13 @@
 package com.example.SpringSecurity.config;
 
+import jakarta.annotation.Nullable;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -21,11 +21,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
 
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, @Nullable HttpServletResponse response, @Nullable FilterChain filterChain) throws ServletException, IOException {
         String header = request.getHeader("Authentication");
         if (header == null || !header.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
+            if (filterChain != null) {
+                filterChain.doFilter(request, response);
+            }
             return;
         }
 
@@ -35,7 +38,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, null, null);
 
         SecurityContextHolder.getContext().setAuthentication(auth);
-
-        filterChain.doFilter(request, response);
+        if (filterChain != null) {
+            filterChain.doFilter(request, response);
+        }
     }
 }
